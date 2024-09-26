da8cc2651a39799b998e0c4bc7523c11747463ba
Christos Matzoros <christosmatzoros@gmail.com>
2024-09-26 11:21:44 +0200

    Initial commit: Add simple C++ script to build CI

---
+++ build/CMakeFiles/cpp_ci_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_ci_example.dir/main.cpp.o"
+  "CMakeFiles/cpp_ci_example.dir/main.cpp.o.d"
+  "cpp_ci_example"
+  "cpp_ci_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_ci_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cpp_ci_example.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpp_ci_example.
+# This may be replaced when dependencies are built.
